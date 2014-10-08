ef2751811a0d4a99f0a10b3daf41616aa62194cc
phitchcock <phitchcock@me.com>
2014-10-08 11:32:33 -0700

    added die class and all labels with TapGest

---
+++ Farkle/DieLabel.swift
@@ -0,0 +1,17 @@
+//
+//  DieLabel.swift
+//  Farkle
+//
+//  Created by Peter Hitchcock on 10/8/14.
+//  Copyright (c) 2014 Peter Hitchcock. All rights reserved.
+//
+
+import UIKit
+
+class DieLabel: UILabel {
+
+    @IBAction func onTapped(sender: UITapGestureRecognizer) {
+        
+    }
+
+}
